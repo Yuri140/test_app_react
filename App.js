8a1d475b588eea3f9c3c8718782433f21547fe2b
Yuri140 <yurisousa.carneiro2006@gmail.com>
2023-08-21 18:15:28 -0300

    alguem me salva

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
-import react from 'react';
-import { NavigationContainer } from "@react-Navigation/native";
-import { createStackNavigation, createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
 
-const stack = createStackNavigator()
+import Task from "./src/Pages/Task/"
+
+const Stack = createStackNavigator()
 
 export default function App() {
 
   return (
-    <NavigationContainer>
+    <NavigationContainer style={styles.container}>
       <Stack.Navigator initialRouteName="Task">
-
+        <Stack.Screen
+          name="Task"
+          component={Task}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
